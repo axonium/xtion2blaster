8b844e838ae41fd31b6df3dfd915b5d237eb6b36
axonium <axonium@gmail.com>
2014-03-04 23:11:28 +0100

    1st try
    Send init signals

--- PWM.c
+++ PWM.c
@@ -1,48 +1,71 @@
-#include
-#include
-#include
- 
-#define DELAY 70000
-#define LEVELS 7
-#define INCREMENT  0.15
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define DELAY 50000
+#define LEVELS 50
+#define INCREMENT  0.001
     FILE *fp;
  
-void  INThandler(int sig)
+/*void  INThandler(int sig)
 {
-    int pin;
+    int pin=0;
         signal(sig, SIG_IGN);
     //turn all pins off
         for (pin = 0; pin < 8; pin++){
         fprintf(fp, "%i=%f\n", pin,0.0);
         fflush(fp);
-    }
+        }
     fclose(fp);
         exit(0);
 }
  
+ */
+
 int main(void)
 {
-    signal(SIGINT, INThandler);
+    //signal(SIGINT, INThandler);
  
-    // arrays used to control brightness of a row of LEDs. left then right animation.
-    float left[] = {0,0,0,0,0,0,0,0,1,0.75,0.375,0.18,0.09,0.04,0.005,0,0,0,0,0,0,0,0};
-    float right[] = {0,0,0,0,0,0,0,0,0.005,0.04,0.09,0.18,0.375,0.75,1,0,0,0,0,0,0,0,0};
- 
-    int pin = 0;
-    float pwm = 0.0;
+    // arrays used to control brightness of a row of LEDs. YAW then THR animation.
+    //float THR[] = {0,0,0.049,0.049,0.049,0.053,0.06,0.049};
+    //float YAW[] = {0,0,0.099,0.099,0.074,0.074,0.074,0.074};
+    int thr = 24;
+    int yaw = 25;
+  //int pit = 23;
+  //int rol = 18;
+    int t = 0;
+    float p = 0.00;
+    //float pwm = 0.0;
  
     // open the pi-blaster device file. If it fails, confirm that pi-blaster has been started
     fp = fopen("/dev/pi-blaster", "w");
     if (fp == NULL) {
-        printf("Error opening file\n");
+        printf("Error opening file \n");
         exit(0);
      }
- 
+    
+
+        fprintf(fp, "%i=%f\n", yaw,0.099 );
+        fprintf(fp, "%i=%f\n", thr,0.049 );
+        fflush(fp);
+        sleep(5);
+    
+    
+    for (p= 0.00 ; p < 0.02 ; p+=INCREMENT){
+        fprintf(fp, "%i=%f\n", yaw,0.072 );
+        fprintf(fp, "%i=%f\n", thr,0.049 + p );
+        fflush(fp);
+        sleep(2);
+    
+    }
+    
+    
     // create the PWM values which are used for the animation going in one direction
-    float pwdPower[LEVELS*4];
-    float i;
-    int j;
-    int count = 7;
+    //float pwdPower[LEVELS];
+    //float i;
+    //int j;
+    /*int count = 7;
  
     for (j = 0; j < LEVELS ;  j++){
         pwdPower[j] = 0.0;
@@ -62,16 +85,21 @@ int main(void)
         for (j = 0; j < (LEVELS) ;  j++){
                 pwdPower[count] = 0.0;
         count++;
-        }
+        } */
  
-///////////////////////////////////////////////////////////////////
+/*//////////////////////////////////////////////////////////////////
  
-    //This section illustrates different brightness levels
+    //This section illustrates different bTHRness levels
     // using PWM with a 2 second puase between levels.
  
     // Enable all pins using the value 0.002 for PWM
+    
+    
+    
+    
+    
     for (pin = 0; pin < 8; pin++){
-                fprintf(fp, "%i=%f\n", pin,0.002);
+                fprintf(fp, "%i=%f\n", yaw ,0.002);
                        fflush(fp);
     }
     sleep(2);
@@ -94,13 +122,18 @@ int main(void)
     // This section will cycle through the power levels 5 times.
     for (j = 0; j < 5 ; j++){
         for (i = 0.0; i < 1 ; i +=INCREMENT){
-            for (pin = 0; pin < 8; pin++){                       fprintf(fp, "%i=%f\n", pin,i );                             fflush(fp);             }       usleep(DELAY);      }               for (i = 1; i >0.00001 ; i -=INCREMENT){
+            for (pin = 0; pin < 8; pin++){
+                    fprintf(fp, "%i=%f\n", pin,i );
+                    fflush(fp);
+            }
+                        usleep(DELAY);
+        }
+        for (i = 1; i >0.00001 ; i -=INCREMENT){
             for (pin = 0; pin < 8; pin++){
                         fprintf(fp, "%i=%f\n", pin,i );
                             fflush(fp);
             }
         usleep(DELAY);
- 
         }
     }
 ///////////////////////////////////////////////////////////////////
@@ -108,16 +141,17 @@ int main(void)
     // The below section performs the animations
     int c = 0;
     int g;
-    for(g = 0; g < 2; g++){      for(c = 16; c>= 0; c--){
+    for(g = 0; g < 2; g++){
+        for(c = 16; c>= 0; c--){
             for (pin = 0; pin < 8  ; pin++){
-                            fprintf(fp, "%i=%f\n", pin, right[pin+c]);
+                            fprintf(fp, "%i=%f\n", pin, THR[pin+c]);
                             fflush(fp);
             }
         usleep(DELAY);
         }
         for(c = 0; c < 16; c++){
             for (pin = 0; pin < 8; pin++){
-                        fprintf(fp, "%i=%f\n", pin, left[pin+c]);
+                        fprintf(fp, "%i=%f\n", pin, YAW[pin+c]);
                             fflush(fp);
             }
         usleep(DELAY);
@@ -138,7 +172,8 @@ int main(void)
         for (pin = 0; pin < 8; pin++){
         fprintf(fp, "%i=%f\n", pin,0.0);
         fflush(fp);
-    }
-    fclose(fp);
+    }*/
+
+ fclose(fp);
     return 0;
 }
