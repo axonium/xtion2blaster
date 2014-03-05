252238efeae0e2b0168a089da9d9b7ac5de756b6
axonium <axonium@gmail.com>
2014-03-05 19:54:46 +0100

    limit flush + stop

--- PWM.cpp
+++ PWM.cpp
@@ -17,21 +17,20 @@ int main(void)
 {       
        
         fprintf(fp, "%i=%f\n", 25,0.099 );
-        fflush(fp);
         fprintf(fp, "%i=%f\n", 24,0.049 );
-        fflush(fp);
         sleep(5) ;
+    
         fprintf(fp, "%i=%f\n", 25,0.072 );
-        fflush(fp);
         fprintf(fp, "%i=%f\n", 24,0.049 );
-        fflush(fp);
         sleep(4);
+    
         for (double p = 0.049; p <0.055 ; p+=INCREMENT ){
             fprintf(fp, "%i=%f\n", 24,p );
-            fflush(fp);
             sleep(1);
         }
-     
-}   
+    fprintf(fp, "%i=%f\n", 24,0.049 ); 
+}
+    
+    fflush(fp);
    return 0;
 }
