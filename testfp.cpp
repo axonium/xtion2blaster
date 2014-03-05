d6d3020cbb52a2f1a0bc9a6a4c02d3a8068c0e5d
axonium <axonium@gmail.com>
2014-03-05 12:06:42 +0100

    Read & Sleep
    test fp + read + sleep

--- testfp.cpp
+++ testfp.cpp
@@ -5,11 +5,31 @@ using namespace std;
 
 int main(void)
 {
-    FILE *fp;
-   fp = fopen("/home/pi/pi-blastert", "w");
-   fprintf(fp, "%i=%f\n", 25,0.099 );
-   fprintf(fp, "%i=%f\n", 24,0.049 );
+   FILE *fp;
+   fp = fopen("/home/pi/pi-blastert", "r+");
+   
+   if(fp)
+{       
+        string ligne;
+        fprintf(fp, "%i=%f\n", 25,0.099 );
+        fprintf(fp, "%i=%f\n", 24,0.049 );
+        while(getline(fp, ligne))  // tant que l'on peut mettre la ligne dans "contenu"
+        {
+                cout << ligne << endl;  // on l'affiche
+                sleep(4);
+        }
+        fprintf(fp, "%i=%f\n", 25,0.072 );
+        fprintf(fp, "%i=%f\n", 24,0.049 );
+        while(getline(fp, ligne))  // tant que l'on peut mettre la ligne dans "contenu"
+        {
+                cout << ligne << endl;  // on l'affiche
+                sleep(4);
+        }
+}
+   
+   
+  
    fflush(fp);
-    
+   
    return 0;
 }
