57e4331d90fd1b5534ecdd0002015056d2fb75fe
axonium <axonium@gmail.com>
2014-03-05 14:37:08 +0100

    Copy and sleep

--- testfp.cpp
+++ testfp.cpp
@@ -1,27 +1,29 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <iostream>
 #include <fstream>
+#include <System.IOUtils.hpp>
 
 using namespace std;
 
 int main(void)
 {
    FILE *fp;
-   fp = fopen("/home/pi/pi-blastert", "r+");
+   fp = fopen("/home/pi/piblastout/pi-blastert", "r+");
    
    if(fp)
 {       
         string ligne;
         fprintf(fp, "%i=%f\n", 25,0.099 );
         fprintf(fp, "%i=%f\n", 24,0.049 );
-        printf(fp); 
+        TFile::Copy("/home/pi/piblastout/pi-blastert", "/home/pi/piblastout/out1") 
         sleep(4) ;
         fprintf(fp, "%i=%f\n", 25,0.072 );
         fprintf(fp, "%i=%f\n", 24,0.049 );
-        printf(fp);
-        sleep(5);
+        TFile::Copy("/home/pi/piblastout/pi-blastert", "/home/pi/piblastout/out2")  
+       
+       
 }   
    fflush(fp);
    return 0;
